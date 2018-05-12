9b3ebca47ef6b79175fc42e3e3ecb9d1b522ae61
Shivani <shivanishah96@hotmail.co..uk>
2018-05-12 01:38:03 +0100

    added redux and packages and added first form

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App'
+import App from './App';
+import { Provider, applyMiddleware, compose } from 'react-redux';
+import { createStore } from 'redux';
+import reduxThunk from 'redux-think';
+import reduxLogger from 'redux-logger'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+const store = createStore(
+    (state = {}) => state,
+    compose(
+        applyMiddleware(reduxThunk,reduxLogger),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,document.getElementById('root'));
 
 registerServiceWorker();
