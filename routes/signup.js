936a0a45f110dcd19949fb65ffd6e2f69e84cecc
Shivani <shivanishah96@hotmail.co..uk>
2018-05-13 03:12:19 +0100

    fixed uniqueness validation

--- routes/signup.js
+++ routes/signup.js
@@ -1,24 +1,50 @@
 import  express from 'express' ;
-import  validateInput from '../src/shared/validations/signup';
+import  commonValidations from '../src/shared/validations/signup';
 let router = express.Router();
 import bcrypt from 'bcrypt';
 import axios from 'axios';
+import isEmpty from 'lodash/isEmpty'
+
+function validateInput(data, otherValidations) {
+    let { errors } = otherValidations(data);
+    return axios.get(`https://serene-waters-30997.herokuapp.com/api/calendar/usersbyname/${data.username}`)
+        .then(user => {
+            if (!isEmpty(user.data)){
+                errors.username = 'There is a user with this user name';
+            }
+            return {
+                errors,
+                isValid: isEmpty(errors)
+            }
+        })
+        .catch(err =>{
+            return {
+            errors,
+            isValid: isEmpty(errors)
+        }})
+
+}
 
 router.post('/api/signup', (req,res)=>{
-    const {errors, isValid } =  validateInput(req.body);
-    if (isValid) {
-        const { username, password, timezone, email } = req.body;
-        const password_digest = bcrypt.hashSync(password, 10);
-        const userData = {
-            username: username,
-            password: password_digest
-        }
-        axios.post('https://serene-waters-30997.herokuapp.com/api/calendar/users', userData)
-            .then(user => res.json({success: true}))
-            .catch(err => res.status(500).json({error:err}))
-    }else{
-        res.status(400).json(errors);
-    }
+   validateInput(req.body, commonValidations).then(({errors, isValid}) =>{
+       if (isValid) {
+
+           const { username, password } = req.body;
+           const password_digest = bcrypt.hashSync(password, 10);
+           const userData = {
+               username: username,
+               password: password_digest
+           }
+           axios.post('https://serene-waters-30997.herokuapp.com/api/calendar/users', userData)
+                .then(user => res.json({success: true}))
+                .catch(err => res.status(500).json({error:err}))
+       }else{
+           res.status(400).json(errors);
+       }
+   })
+
+
+
 
 });
 
